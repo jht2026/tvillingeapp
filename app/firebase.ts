d33833479f83653a8a1ccbe2bf2e963d50952891
Josefine Heidemann Sørensen <jht@cworldwide.com>
2026-05-14 19:18:24 +0200

    fix firebase init

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { getApps, initializeApp } from 'firebase/app';
 import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -10,11 +10,7 @@ const firebaseConfig = {
   appId: "1:48267119661:web:ddfbfb62d93457b3013ca4",
 };
 
-if (typeof window !== 'undefined') {
-  (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = false;
-}
-
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
 });
