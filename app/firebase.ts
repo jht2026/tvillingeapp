3b5de41baf6feb3a67da6b0794c16e6b121f34ef
Josefine Heidemann Sørensen <jht@cworldwide.com>
2026-06-23 09:28:33 +0200

    Fix firebase

--- app/firebase.ts
+++ app/firebase.ts
@@ -14,14 +14,3 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
 });
-
-// Deaktiver Firebase debug knap
-if (typeof window !== 'undefined') {
-  setTimeout(() => {
-    const debugBtn = document.querySelector('firebase-app-check-debug');
-    if (debugBtn) {
-      (debugBtn as HTMLElement).style.pointerEvents = 'none';
-      (debugBtn as HTMLElement).style.opacity = '0.3';
-    }
-  }, 2000);
-}
